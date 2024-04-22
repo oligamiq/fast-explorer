4ce6754d2976466bf0ef0680a210e3cb7dfe4da8
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-04-22 15:54:34 +0900

    add image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use global_hotkey::{
+    hotkey::{Code, HotKey, Modifiers},
+    GlobalHotKeyEvent, GlobalHotKeyManager, HotKeyState,
+};
+use tray_icon::{menu::Menu, TrayIconBuilder};
+use winit::event_loop::{ControlFlow, EventLoopBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/icon.png");
+
+    let event_loop = EventLoopBuilder::new().build().unwrap();
+
+    let hotkeys_manager = GlobalHotKeyManager::new().unwrap();
+
+    let hotkey = HotKey::new(Some(Modifiers::SHIFT), Code::KeyD);
+    let hotkey2 = HotKey::new(Some(Modifiers::SHIFT | Modifiers::ALT), Code::KeyD);
+    let hotkey3 = HotKey::new(None, Code::KeyF);
+
+    hotkeys_manager.register(hotkey).unwrap();
+    hotkeys_manager.register(hotkey2).unwrap();
+    hotkeys_manager.register(hotkey3).unwrap();
+
+    let global_hotkey_channel = GlobalHotKeyEvent::receiver();
+
+    event_loop
+        .run(move |_event, event_loop| {
+            event_loop.set_control_flow(ControlFlow::Poll);
+
+            if let Ok(event) = global_hotkey_channel.try_recv() {
+                println!("{event:?}");
+
+                if hotkey2.id() == event.id && event.state == HotKeyState::Released {
+                    hotkeys_manager.unregister(hotkey2).unwrap();
+                }
+            }
+        })
+        .unwrap();
+
+    let tray_menu = Menu::new();
+    let tray_icon = TrayIconBuilder::new()
+        .with_menu(Box::new(tray_menu))
+        .with_tooltip("system-tray - tray icon library!")
+        .with_icon(icon)
+        .build()
+        .unwrap();
 }
