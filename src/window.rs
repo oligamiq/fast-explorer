685265480af26d9a6a64a0d55bf51f3998ba19a3
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-04-29 19:07:42 +0900

    rm

--- src/window.rs
+++ src/window.rs
@@ -12,10 +12,6 @@ impl WindowWrapper {
         let mut window = Window::default_attributes();
         window.title = "FastExplorer".into();
         // let window = window.with_active(false);
-        // let window = window.with_enabled_buttons(WindowButtons::empty());
-
-        // window;
-
 
         let window = event_loop
                     .create_window(window)
@@ -24,37 +20,8 @@ impl WindowWrapper {
         window.focus_window();
 
         let hwnd: u64 = window.id().into();
-        let title = window.title();
-
-        let hwnd_ptr = hwnd as *mut c_void;
-        let app_window = unsafe { windows::UI::WindowManagement::AppWindow::from_raw_borrowed(&hwnd_ptr) };
-
-        // println!("title: {:?}", app_window.unwrap().Title());
-
         let hwnd: HWND = HWND(hwnd as isize);
 
-        // println!("app_window: {:?}", app_window);
-
-        // let title = app_window.Title();
-        // println!("{:?}", title);
-
-        let app_window_ptr = unsafe {
-            GetWindowLongPtrW(hwnd, windows::Win32::UI::WindowsAndMessaging::GWLP_USERDATA)
-        };
-        // AppWindow::
-        // let app_window_ptr = app_window_ptr as *mut c_void;
-        // let app_window2 = unsafe { windows::UI::WindowManagement::AppWindow::from_raw_borrowed(&app_window_ptr) };
-        // let title = app_window2.unwrap().Title();
-
-        // println!("title: {:?}", title);
-
-        // let userdata_ptr = get_app_window_from_hwnd(hwnd).unwrap();
-
-        dbg!(hwnd);
-
-        // unsafe {
-            // AppWindowTitleBar::query(&self, iid, interface)
-        // }
 
         Self { window }
     }
